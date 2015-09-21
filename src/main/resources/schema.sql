52030957b39b4478748d5a429f01b57aee5fa0a4
Carsten Soemer <carsten.soemer@logicline.de>
2015-09-21 20:26:53 +0200

    Generate / update schema from Model Classes using Hibernate

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,2 @@
-DROP TABLE IF EXISTS Person;
+--Place SQL Skripts here
 
-DROP SEQUENCE IF EXISTS hibernate_sequence;
-
-CREATE SEQUENCE hibernate_sequence START WITH 1 INCREMENT BY 1;
-
-CREATE TABLE Person	(
-	id SERIAL PRIMARY KEY,
-  	firstName varchar(255),	
-	lastName varchar(255)
-);
