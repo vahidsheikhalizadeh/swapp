7f9b7f8469f9c2eeda9db0e5dfc13a4ceb7aa7f9
Carsten Soemer <carsten.soemer@logicline.de>
2015-09-21 19:22:39 +0200

    added missing sequence for primary key generation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS Person;
 
+DROP SEQUENCE IF EXISTS hibernate_sequence;
+
+CREATE SEQUENCE hibernate_sequence START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE Person	(
 	id SERIAL PRIMARY KEY,
   	firstName varchar(255),	
